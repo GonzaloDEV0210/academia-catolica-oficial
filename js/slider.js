5426d91f6aa200b3fb34433362eb13a3524b8097
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2024-09-13 23:08:35 -0500

    slider y titulo hecho

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('#sliders', {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+    effect: 'fade',
+    speed: 800
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/*.html", "/js/*.js"],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'azul_catolica': '#1B5CFF',
+        'amarillo_catolica': '#F4DA40',
+        'verde_catolica': '#36A2BC',
+      },
+      colors: {
+        'azul_catolica': '#1B5CFF',
+        'amarillo_catolica': '#F4DA40',
+        'verde_catolica': '#36A2BC',
+      },
+      borderColor: {
+        'azul_catolica': '#1B5CFF',
+        'amarillo_catolica': '#F4DA40',
+        'verde_catolica': '#36A2BC',
+      },
+      fontFamily: {
+        'bree_bold': ['Bree_bold', 'arial'],
+        'bree_regular': ['Bree_regular', 'arial'],
+        'clanpro_news': ['clanpro_news', 'arial'],
+        'helvetic': ['helvetic', 'arial'],
+      },
+      screens: {
+        'celular': '320px',
+        'eg': '1600px',
+      },
+      padding: {
+        '5%': '8%'
+      },
+    },
   },
   plugins: [],
 }
